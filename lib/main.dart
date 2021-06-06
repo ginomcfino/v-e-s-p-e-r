7f9bca34220eb58a3675b32a5bfbbaabbe978d33
Weiqi Ji <jiweiqi717@gmail.com>
2021-06-05 22:27:57 -0500

    this is hilarious - wordPair

--- lib/main.dart
+++ lib/main.dart
@@ -33,3 +33,16 @@ class HomePage extends StatelessWidget {
     );
   }
 }
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
