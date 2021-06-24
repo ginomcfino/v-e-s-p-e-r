fad967d3ffe29eea521719cc27b7b1611914e809
Weiqi Ji <jiweiqi717@gmail.com>
2021-06-24 02:49:15 -0500

    added fonts

--- lib/main.dart
+++ lib/main.dart
@@ -168,3 +168,12 @@ class _RandomWordsState extends State<RandomWords> {
         });
   }
 }
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
